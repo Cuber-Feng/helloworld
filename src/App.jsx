3089299f3b3918ca4059e5665afc0be7fb013af1
Feng Yuxiang <2017feng35@gmail.com>
2026-09-25 17:43:49 +0800

    Time zone

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 
 import Clock from './components/Clock';
@@ -5,14 +6,12 @@ import Card from './components/Card';
 import Banner from './components/Banner';
 
 function App() {
-  // const [theme, setTheme] = useState("dark");
   const [theme, setTheme] = useState(() => {
     return window.__INITIAL_THEME__ || 'light';
   });
 
   // 逻辑 1：当 theme 改变时，真正去修改网页的属性
   useEffect(() => {
-    // 关键：把 theme 字符串应用到 html 标签的属性上
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]); // 注意：这里的依赖项是 [theme]
 
@@ -20,6 +19,7 @@ function App() {
   useEffect(() => {
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setTheme(mediaQuery.matches ? 'dark' : 'light');
 
     const handleChange = (e) => {
